a241b04102699f9f8dbf8efe7dc85ba91889c978
mvee18 <emvalenc@go.olemiss.edu>
2021-11-20 13:41:55 -0600

    updated database schema and register

--- backend/flaskr/schema.sql
+++ backend/flaskr/schema.sql
@@ -4,15 +4,24 @@ DROP TABLE IF EXISTS post;
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  total INTEGER
+  user2 TEXT,
+  user3 TEXT,
+  user4 TEXT,
+  curscore INTEGER,
+  totalscore INTEGER
 );
 
 CREATE TABLE stations (
   username TEXT,
+  clue TEXT,
   station TEXT,
   answer1 TEXT,
   answer2 TEXT,
-  score INTEGER,
+  canswer1 TEXT,
+  canswer2 REAL,
+  score1 INTEGER,
+  score2 INTEGER,
   attempt INTEGER,
+  percentError REAL,
   FOREIGN KEY (username) REFERENCES user (username)
 );
